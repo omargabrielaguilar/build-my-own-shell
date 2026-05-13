965493deb2f1dba6f9ec115ccc37278176b76346
Omar <omareegab@gmail.com>
2026-05-13 15:36:14 -0500

    codecrafters submit [skip ci]

---
+++ annotations.md
@@ -0,0 +1,15 @@
+repl: 
+
+read
+eval
+print
+loop
+
+
+type command 
+builting is used to determine how a command would be interpreted if it were used
+
+examplete  check command is :
+    builtin
+    executable file
+    unrecognized
--- app/main.go
+++ app/main.go
@@ -15,19 +15,33 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		command, _ := reader.ReadString('\n')
+		builtinToCheck := command[5:]
 
-		// if i want to exit the shell
+		// 1. if i want to exit the shell
 		command = strings.TrimSpace(command)
 		if command == "exit" {
 			break
 		}
 
-		// if i want echo to my command, method hasPrefix helps to read before the argument passed
+		// 2.  if i want echo to my command, method hasPrefix helps to read before the argument passed
 		if strings.HasPrefix(command, "echo ") {
-			fmt.Println(command[5:])
+			fmt.Println(builtinToCheck)
 		} else {
 			fmt.Println(command + ": command not found")
 		}
 
+		// slice mode
+		commands := []string{
+			"exit",
+			"echo",
+			"type",
+		}
+
+		// 3. Work with typeee
+		if strings.HasPrefix(command, "type ") {
+			for _, builtinToCheck := range commands {
+				fmt.Println(builtinToCheck + "is a shell builtin")
+			}
+		}
 	}
 }

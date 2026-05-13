f776f4b3c4da561a1fbfabc47a1990b51df3f698
Omar <omareegab@gmail.com>
2026-05-13 15:44:02 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -15,20 +15,6 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		command, _ := reader.ReadString('\n')
-		builtinToCheck := command[5:]
-
-		// 1. if i want to exit the shell
-		command = strings.TrimSpace(command)
-		if command == "exit" {
-			break
-		}
-
-		// 2.  if i want echo to my command, method hasPrefix helps to read before the argument passed
-		if strings.HasPrefix(command, "echo ") {
-			fmt.Println(builtinToCheck)
-		} else {
-			fmt.Println(command + ": command not found")
-		}
 
 		// slice mode
 		commands := []string{
@@ -37,11 +23,26 @@ func main() {
 			"type",
 		}
 
-		// 3. Work with typeee
-		if strings.HasPrefix(command, "type ") {
-			for _, builtinToCheck := range commands {
-				fmt.Println(builtinToCheck + "is a shell builtin")
-			}
+		// 1. if i want to exit the shell
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
 		}
+
+		// after trim the string
+		builtinToCheck := command[5:]
+
+		// 2.  if i want echo to my command, method hasPrefix helps to read before the argument passed
+		if strings.HasPrefix(command, "echo ") {
+			fmt.Println(builtinToCheck)
+		} else if strings.HasPrefix(command, "type ") {
+			// 3. Work with typeee
+			for _, builtinToCheck := range commands {
+				fmt.Println(builtinToCheck + " is a shell builtin")
+			}
+		} else {
+			fmt.Println(command + ": command not found")
+		}
+
 	}
 }
